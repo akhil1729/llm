115cab4fd54da7581f0c4a2de637283294b258a2
Akhil Kanukula <kanukulaakhil2017@gmail.com>
2025-07-22 13:41:50 -0400

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default {
-    env: {
-      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000",
-    },
-  };
-  
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000",
+  },
+};
+
+export default nextConfig;
